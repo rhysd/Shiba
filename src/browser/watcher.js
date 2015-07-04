d2d921aaf625054f343d77a48e145b3105cfa185
rhysd <lin90162@yahoo.co.jp>
2015-07-05 02:31:30 +0900

    notify the directory change to browser process

--- src/browser/watcher.js
+++ src/browser/watcher.js
@@ -5,29 +5,39 @@ var marked = require('marked');
 var path = require('path');
 var fs = require('fs');
 
-module.exports =
-class Watcher {
-    constructor(d, r) {
-        this.dir = d;
-        this.render = r;
+// Note:
+// ES6 class syntax is unavailable for 'remote' module in renderer process
+function Watcher(d, r) {
+    this.dir = d;
+    this.render = r;
 
-        // XXX: Temporary
-        const f = path.join(this.dir, 'README.md');
-        if (fs.statSync(f).isFile()) {
-            this.update(f);
+    // XXX: Temporary
+    const f = path.join(this.dir, 'README.md');
+    if (fs.statSync(f).isFile()) {
+        this.update(f);
+    }
+}
+
+Watcher.prototype.update = function(file) {
+    let that = this;
+    // Encoding should be specified by config or detected
+    fs.readFile(file, 'utf-8', function(err, text){
+        if (err) {
+            console.log("Can't open: " + file);
+            return;
         }
-    }
 
-    update(file) {
-        let that = this;
-        // Encoding should be specified by config or detected
-        fs.readFile(file, 'utf-8', function(err, text){
-            if (err) {
-                console.log("Can't open: " + file);
-                return;
-            }
-
-            that.render(marked(text));
-        });
-    }
+        that.render(marked(text));
+    });
 };
+
+Watcher.prototype.changeWatchingDir = function(new_dir) {
+    if (new_dir === this.dir) {
+        return;
+    }
+
+    this.dir = new_dir;
+    // TODO
+};
+
+module.exports = Watcher;
