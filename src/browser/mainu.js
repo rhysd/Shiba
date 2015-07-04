a296903f0cfb3778ca087ea28bd4c1011a76400b
rhysd <lin90162@yahoo.co.jp>
2015-07-04 14:40:16 +0900

    first app

---
+++ src/browser/mainu.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var app = require('app');
+var path = require('path');
+var ipc = require('ipc');
+var shortcut = require('global-shortcut');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+// Main Window {{{
+var mainWindow = null;
+
+app.on('window-all-closed', function(){ app.quit(); });
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    const html = 'file://' + path.resolve(__dirname, '..', '..', 'static', 'index.html');
+    mainWindow.loadUrl(html);
+
+    mainWindow.openDevTools();
+
+    mainWindow.on('closed', function(){
+        mainWindow = null;
+        shortcut.unregisterAll();
+    });
+});
+// }}}
+
