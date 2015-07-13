f24d1fff3453506cbc18889f86ac6a6a7f9e826a
rhysd <lin90162@yahoo.co.jp>
2015-07-13 21:55:21 +0900

    fix cli.js and bump up version to 0.1.1

--- bin/cli.js
+++ bin/cli.js
@@ -5,10 +5,22 @@
 var child_process = require('child_process');
 var electron = require('electron-prebuilt');
 var path = require('path');
+var fs = require('fs');
 
-var mainu = path.join(__dirname, '..', 'src', 'browser', 'mainu.js');
-var argv = process.argv;
-argv.unshift(mainu);
-console.log(argv);
+var argv = [path.join(__dirname, '..', 'src', 'browser', 'mainu.js')];
+var len = process.argv.length;
+
+// First is 'node' and Second arg is '/path/to/bin/shiba'.
+// If user specifies argument, the length of argv must be more than 2.
+if (len > 2) {
+    var last_arg = process.argv[len-1];
+    if (fs.existsSync(last_arg)) {
+        argv.push(last_arg);
+    } else {
+        argv.push(process.cwd());
+    }
+} else {
+    argv.push(process.cwd());
+}
 
 child_process.spawn(electron, argv);
