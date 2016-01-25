f0edd2d362ea3fb705e971d1d2f800777406e0a9
rhysd <lin90162@yahoo.co.jp>
2016-01-25 19:01:28 +0900

    fix test code following Electron update

--- tests/browser/menu_test.ts
+++ tests/browser/menu_test.ts
@@ -1,11 +1,11 @@
-import BW = require('browser-window');
+import {BrowserWindow} from 'electron';
 import * as M from '../../browser/menu';
 
 context('menu', () => {
     describe('#build', () => {
         it('returns menu object', () => {
             if (on_travis) {
-                let w = new BW({show: false});
+                let w = new BrowserWindow({show: false});
                 const m = M.build(w);
                 assert.ok(m);
             }
