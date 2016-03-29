9ae153245cdab6f9a67124ce5496a852c8592ed8
rhysd <lin90162@yahoo.co.jp>
2016-03-30 00:14:25 +0900

    use power-assert

--- test/main/initial_path_test.ts
+++ test/main/initial_path_test.ts
@@ -2,7 +2,7 @@
 
 import initial_path = require('../../browser/initial_path');
 import * as path from 'path';
-import {assert} from 'chai';
+import * as assert from 'power-assert';
 
 describe('#initial_path()', () => {
     const argv = process.argv;
@@ -14,29 +14,29 @@ describe('#initial_path()', () => {
 
     it('returns cwd when no argument is specified in general platform', () => {
         process.argv = [];
-        assert.strictEqual(initial_path(), process.cwd());
+        assert(initial_path() === process.cwd());
     });
 
     it('returns document directory if started with Shiba.app in darwin', () => {
         if (process.platform === 'darwin') {
             process.argv = [];
             process.chdir('/');
-            assert.match(initial_path(), /Documents$/);
+            assert(/Documents$/.test(initial_path()));
         }
     });
 
     it('returns the specified path if it exists', () => {
         process.argv = ['dummy', './README.md'];
-        assert.strictEqual(initial_path(), path.resolve('./README.md'));
+        assert(initial_path() === path.resolve('./README.md'));
     });
 
     it('returns default path if specified path does not exist', () => {
         process.argv = ['dummy', '/this/file/does/not/exist'];
-        assert.strictEqual(initial_path(), process.cwd());
+        assert(initial_path() === process.cwd());
     });
 
     it('returns the last argument of argv if multiple argument specified', () => {
         process.argv = ['dummy', 'foo', 'bar', './README.md'];
-        assert.strictEqual(initial_path(), path.resolve('./README.md'));
+        assert(initial_path() === path.resolve('./README.md'));
     });
 });
