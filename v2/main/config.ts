d52078133a7bcccf4d1c8f764f0c3d699887f411
rhysd <lin90162@yahoo.co.jp>
2016-10-09 10:59:06 +0900

    watch config file and detect its update automatically

--- v2/main/config.ts
+++ v2/main/config.ts
@@ -1,7 +1,8 @@
-import * as yaml from 'js-yaml';
 import * as path from 'path';
 import {readFileSync, writeFileSync} from 'fs';
-import {app} from 'electron';
+import * as yaml from 'js-yaml';
+import * as chokidar from 'chokidar';
+import {app, BrowserWindow} from 'electron';
 import log from './log';
 import Ipc from './ipc';
 
@@ -73,38 +74,56 @@ function loadConfigFromFile(dir: string): AppConfig | null {
     }
 }
 
+function loadOrCreateConfigFile(dir: string) {
+    const config = loadConfigFromFile(dir);
+    if (config !== null) {
+        config._config_dir_path = dir;
+        if (config.drawer) {
+            log.warn("'drawer' option was removed. It'll be ignored.");
+        }
+        if (config.markdown) {
+            log.warn("Deprecated configration 'markdown' will be converted to 'preview_customize.markdown'");
+            config.preview_customize = {
+                markdown: config.markdown,
+            };
+        }
+        log.debug('Configuration was loaded from', dir, config);
+        global.application_config = config;
+        return config;
+    } else {
+        const file = path.join(dir, 'config.yaml');
+        writeFileSync(file, yaml.safeDump(DEFAULT_CONFIG));
+        log.info('New configuration file created:', file);
+        DEFAULT_CONFIG._config_dir_path = dir;
+        global.application_config = DEFAULT_CONFIG;
+        return config;
+    }
+}
+
 export default function loadAppConfig() {
     return new Promise<AppConfig>(resolve => {
         if (global.application_config) {
             return resolve(global.application_config);
         }
-
         const dir = app.getPath('userData');
-        const config = loadConfigFromFile(dir);
-        if (config !== null) {
-            config._config_dir_path = dir;
-            if (config.drawer) {
-                log.warn("'drawer' option was removed. It'll be ignored.");
-            }
-            if (config.markdown) {
-                log.warn("Deprecated configration 'markdown' will be converted to 'preview_customize.markdown'");
-                config.preview_customize = {
-                    markdown: config.markdown,
-                };
-            }
-            log.debug('Configuration was loaded from', dir, config);
-            global.application_config = config;
-            resolve(config);
-        } else {
-            const file = path.join(dir, 'config.yaml');
-            writeFileSync(file, yaml.safeDump(DEFAULT_CONFIG));
-            log.info('New configuration file created:', file);
-            DEFAULT_CONFIG._config_dir_path = dir;
-            global.application_config = DEFAULT_CONFIG;
-            resolve(DEFAULT_CONFIG);
-        }
+        resolve(loadOrCreateConfigFile(dir));
+
         Ipc.onReceive('shiba:request-config', function (this: Electron.IpcMainEvent) {
             this.sender.send('shiba:send-config', global.application_config);
+        });
+
+        chokidar.watch(path.join(dir, 'config.(yaml|json)'), {
+            ignoreInitial: true,
+            persistent: true,
+        }).on('add', () => {
+            const config = loadOrCreateConfigFile(dir);
+            log.debug('Configuration was update. Will send a new configuration to renderer process', config);
+            const wins = BrowserWindow.getAllWindows();
+            if (wins.length === 0) {
+                log.warn('No browser window was found! Updating configuration will be skipped');
+                return;
+            }
+            wins[0].webContents.send('shiba:send-config', config);
         });
     });
 }
