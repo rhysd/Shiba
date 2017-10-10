307daa950d3df5cac929bf484124f4c795c6f61e
rhysd <lin90162@yahoo.co.jp>
2017-10-10 21:35:39 +0900

    fix and improve tests for initial_path()

--- test/main/initial_path_test.ts
+++ test/main/initial_path_test.ts
@@ -4,12 +4,22 @@ import initial_path = require('../../browser/initial_path');
 import * as path from 'path';
 import * as assert from 'power-assert';
 
+function argv(...a: string[]) {
+    return ['path/to/Electron', 'path/to/Shiba', ...a];
+}
+
 describe('#initial_path()', () => {
-    const argv = process.argv;
-    const cwd = process.cwd();
+    let argv_save: string[];
+    let cwd_save: string;
+
+    beforeEach(() => {
+        argv_save = process.argv;
+        cwd_save = process.cwd();
+    });
+
     afterEach(() => {
-        process.argv = argv;
-        process.chdir(cwd);
+        process.argv = argv_save;
+        process.chdir(cwd_save);
     });
 
     it('returns cwd when no argument is specified in general platform', () => {
@@ -26,17 +36,17 @@ describe('#initial_path()', () => {
     });
 
     it('returns the specified path if it exists', () => {
-        process.argv = ['dummy', './README.md'];
+        process.argv = argv('./README.md');
         assert(initial_path() === path.resolve('./README.md'));
     });
 
     it('returns default path if specified path does not exist', () => {
-        process.argv = ['dummy', '/this/file/does/not/exist'];
+        process.argv = argv('/this/file/does/not/exist');
         assert(initial_path() === process.cwd());
     });
 
     it('returns the last argument of argv if multiple argument specified', () => {
-        process.argv = ['dummy', 'foo', 'bar', './README.md'];
+        process.argv = argv('foo', 'bar', './README.md');
         assert(initial_path() === path.resolve('./README.md'));
     });
 });
