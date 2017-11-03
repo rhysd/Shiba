57e116bb185669037e9de878a54f710f993ac1bb
rhysd <lin90162@yahoo.co.jp>
2017-11-04 01:06:13 +0900

    add test for the case where Electron binary is directly executed

--- test/main/initial_path_test.ts
+++ test/main/initial_path_test.ts
@@ -23,13 +23,13 @@ describe('#initial_path()', () => {
     });
 
     it('returns cwd when no argument is specified in general platform', () => {
-        process.argv = [];
-        assert(initial_path('') === process.cwd());
+        process.argv = argv();
+        assert.equal(initial_path(''), process.cwd());
     });
 
     it('returns document directory if started with Shiba.app in darwin', () => {
         if (process.platform === 'darwin') {
-            process.argv = [];
+            process.argv = argv();
             process.chdir('/');
             assert(/Documents$/.test(initial_path('')));
         }
@@ -37,16 +37,29 @@ describe('#initial_path()', () => {
 
     it('returns the specified path if it exists', () => {
         process.argv = argv('./README.md');
-        assert(initial_path('') === path.resolve('./README.md'));
+        assert.equal(initial_path(''), path.resolve('./README.md'));
     });
 
     it('returns default path if specified path does not exist', () => {
         process.argv = argv('/this/file/does/not/exist');
-        assert(initial_path('') === process.cwd());
+        assert.equal(initial_path(''), process.cwd());
     });
 
     it('returns the last argument of argv if multiple argument specified', () => {
         process.argv = argv('foo', 'bar', './README.md');
-        assert(initial_path('') === path.resolve('./README.md'));
+        assert.equal(initial_path(''), path.resolve('./README.md'));
+    });
+
+    it('considers the case where the Electron binary is directly executed', () => {
+        let exe = '/path/to/Shiba';
+        if (process.platform === 'win32') {
+            exe = 'C:\\path\\to\\Shiba.exe';
+        }
+
+        process.argv = [exe];
+        assert.equal(initial_path(''), process.cwd());
+
+        process.argv = [exe, './README.md'];
+        assert.equal(initial_path(''), path.resolve('./README.md'));
     });
 });
