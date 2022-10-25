b19551f72a3201ca8076c00637dbc8abac9efda4
rhysd <lin90162@yahoo.co.jp>
2022-10-26 00:52:13 +0900

    move CLI parser logic to library

--- v2/src/cli.rs
+++ v2/src/cli.rs
@@ -1,9 +1,26 @@
+use anyhow::Result;
+use getopts::Options as GetOpts;
 use std::path::PathBuf;
-
-// TODO: Move CLI parser here
 
 #[derive(Debug, Default)]
 pub struct Options {
     pub debug: bool,
     pub init_file: Option<PathBuf>,
+}
+
+impl Options {
+    pub fn from_args(iter: impl Iterator<Item = String>) -> Result<Option<Self>> {
+        let mut opts = GetOpts::new();
+        opts.optflag("h", "help", "print this help");
+        let matches = opts.parse(iter)?;
+        if matches.opt_present("h") {
+            println!("{}", opts.usage("Usage: shiba [option] FILE"));
+            return Ok(None);
+        }
+
+        Ok(Some(Self {
+            debug: false,
+            init_file: matches.free.into_iter().next().map(PathBuf::from),
+        }))
+    }
 }
--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,13 +1,6 @@
 use anyhow::Result;
-use getopts::Options;
+use shiba_preview::{run, Options};
 use std::env;
-use std::path::PathBuf;
-
-fn usage(options: Options) {
-    let program = env::args().next().unwrap();
-    let header = format!("Usage: {} [option] FILE", program);
-    println!("{}", options.usage(&header));
-}
 
 fn main() -> Result<()> {
     let debug = env::var("SHIBA_DEBUG").is_ok();
@@ -15,17 +8,10 @@ fn main() -> Result<()> {
 
     env_logger::builder().filter_level(level).init();
 
-    let mut options = Options::new();
-    options.optflag("h", "help", "print this help");
-    let matches = options.parse(env::args().skip(1))?;
-    if matches.opt_present("h") {
-        usage(options);
-        return Ok(());
+    if let Some(mut options) = Options::from_args(env::args().skip(1))? {
+        options.debug = debug;
+        run(options)?;
     }
 
-    let options = shiba_preview::Options {
-        debug,
-        init_file: matches.free.into_iter().next().map(PathBuf::from),
-    };
-    shiba_preview::run(options)
+    Ok(())
 }
