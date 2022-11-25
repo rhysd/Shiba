32fd5525cb559beb947f7978d13b266bedc7fa1f
rhysd <lin90162@yahoo.co.jp>
2022-11-25 10:39:05 +0900

    implement modest snackbar

--- v2/web/components/Notification.tsx
+++ v2/web/components/Notification.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Snackbar from '@mui/material/Snackbar';
-import SnackbarContent from '@mui/material/SnackbarContent';
 import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import Paper from '@mui/material/Paper';
 import { dismissNotification, type Dispatch } from '../reducer';
 import { sendMessage } from '../ipc';
 
 const ORIGIN = { vertical: 'bottom', horizontal: 'right' } as const;
+const ROOT_STYLE: React.CSSProperties = {
+    bottom: '0',
+    right: '0',
+};
+const BODY_STYLE: React.CSSProperties = {
+    borderTopRightRadius: '0',
+    borderBottomRightRadius: '0',
+    borderBottomLeftRadius: '0',
+    borderRight: '0',
+    borderBottom: '0',
+    padding: '2px 8px',
+};
 
 function zoomIn(): void {
     sendMessage({ kind: 'zoom', zoom: 'In' });
@@ -27,26 +38,18 @@ export const ZoomNotification: React.FC<ZoomProps> = ({ open, percent, dispatch 
     const handleClose = (): void => {
         dispatch(dismissNotification());
     };
-    const action = (
-        <>
-            <IconButton onClick={zoomIn} size="small" aria-label="zoom in" color="info">
-                <ZoomInIcon fontSize="small" />
-            </IconButton>
-            <IconButton onClick={zoomOut} size="small" aria-label="zoom out" color="info">
-                <ZoomOutIcon fontSize="small" />
-            </IconButton>
-            <IconButton onClick={handleClose} size="small" aria-label="close" color="inherit">
-                <CloseIcon fontSize="small" />
-            </IconButton>
-        </>
-    );
-
-    // Keep font size when scale factor is smaller than 1.0
-    const fontSize = open && percent < 100 ? `${100 / percent}em` : '1em';
 
     return (
-        <Snackbar open={open} autoHideDuration={4000} onClose={handleClose} anchorOrigin={ORIGIN}>
-            <SnackbarContent style={{ fontSize }} message={`Zoom: ${percent}%`} action={action} />
+        <Snackbar style={ROOT_STYLE} open={open} autoHideDuration={4000} onClose={handleClose} anchorOrigin={ORIGIN}>
+            <Paper style={BODY_STYLE} variant="outlined">
+                Zoom: {percent}%
+                <IconButton onClick={zoomIn} size="small" aria-label="zoom in" color="info">
+                    <ZoomInIcon fontSize="small" />
+                </IconButton>
+                <IconButton onClick={zoomOut} size="small" aria-label="zoom out" color="info">
+                    <ZoomOutIcon fontSize="small" />
+                </IconButton>
+            </Paper>
         </Snackbar>
     );
 };
