2a9928ee5fe5eb7bf8a17e81535aa037dc2bfb66
rhysd <lin90162@yahoo.co.jp>
2022-12-29 23:28:18 +0900

    fix exit handler is not called when app quits with `Cmd+Q`

--- v2/src/lib.rs
+++ v2/src/lib.rs
@@ -53,14 +53,14 @@ pub fn run(options: Options) -> Result<()> {
             Event::UserEvent(event) => {
                 log::debug!("Handling user event {:?}", event);
                 match app.handle_user_event(event) {
-                    Ok(AppControl::Exit) => *control_flow = ControlFlow::Exit,
-                    Ok(_) => {}
+                    Ok(AppControl::Exit) => true,
+                    Ok(_) => false,
                     Err(err) => {
                         log::error!("Could not handle user event");
                         log_causes(err);
+                        false
                     }
                 }
-                false
             }
             Event::MenuEvent { menu_id, .. } => match app.handle_menu_event(menu_id) {
                 Ok(AppControl::Exit) => true,
