e90eda68546f138819f6a687053c90b51ff981f4
rhysd <lin90162@yahoo.co.jp>
2023-08-22 01:39:47 +0900

    add script to generate Shiba.app on macOS

--- v2/assets/README.md
+++ v2/assets/README.md
@@ -8,8 +8,9 @@ convert shibainu.png -define icon:auto-resize=256,128,48,32,16 icon.ico
 
 ### How to create `icon.icns`
 
-On macOS, run the following command:
+On macOS, run the following commands:
 
 ```sh
 iconutil -c icns icon.iconset
+mv icon.icns ./Shiba.app/Contents/Resources/
 ```
---
+++ v2/scripts/gen_macos_app.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ ! -d ../.git ]; then
+    echo 'ERROR: This script must be run at v2 directory' 1>&2
+    exit 1
+fi
+
+echo "Generating Shiba.app in current directory..."
+rm -rf ./Shiba.app
+
+echo "Detecting version from Cargo.toml..."
+pat='^version \= "([^"]+)"$'
+while IFS=: read -r line; do
+    if [[ "$line" =~ $pat ]]; then
+        version="${BASH_REMATCH[1]}"
+        break
+    fi
+done < ./Cargo.toml
+if [ "$version" = "" ]; then
+    echo 'ERROR: No version was found in Cargo.toml' 1>&2
+    exit 1
+fi
+
+echo "Generating ./Shiba.app for version ${version}..."
+cp -R ./assets/Shiba.app ./
+sed -E -i '' "s/\\{\\{VERSION}}/${version}/" ./Shiba.app/Contents/Info.plist
+
+echo "Copying 'shiba' release binary..."
+if [ ! -x ./target/release/shiba ]; then
+    echo "ERROR: ./target/release/shiba does not exist. Build it with 'cargo build --release'" 1>&2
+    exit 1
+fi
+mkdir -p ./Shiba.app/Contents/MacOS/
+cp ./target/release/shiba ./Shiba.app/Contents/MacOS/
+
+echo 'Done.'
