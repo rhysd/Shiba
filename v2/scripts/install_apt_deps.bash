8b999db2fac7f6d77a603eeccf2e9fa9f75280d1
rhysd <lin90162@yahoo.co.jp>
2023-09-03 23:53:10 +0900

    add `-y` on `apt-get update`

--- v2/scripts/install_apt_deps.bash
+++ v2/scripts/install_apt_deps.bash
@@ -2,5 +2,8 @@
 
 set -e -x -o pipefail
 
-apt-get update
+apt-get update -y
+
+# Install libwebkit2gtk-4.1-dev for wry: https://github.com/tauri-apps/wry#platform-specific-notes
+# Install libgtk-3-dev and libxdo-dev for muda: https://github.com/tauri-apps/muda#dependencies-linux-only
 apt-get install -y --no-install-recommends libwebkit2gtk-4.1-dev libgtk-3-dev libxdo-dev
