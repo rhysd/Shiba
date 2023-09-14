a0a800fcb284800ce8eb3ed7934e8dc91d338486
rhysd <lin90162@yahoo.co.jp>
2023-09-14 22:48:40 +0900

    reduce output from `apt-get` by using `-q`

--- v2/scripts/install_apt_deps.bash
+++ v2/scripts/install_apt_deps.bash
@@ -2,8 +2,8 @@
 
 set -e -x -o pipefail
 
-apt-get update -y
+apt-get update -y -q
 
 # Install libwebkit2gtk-4.1-dev for wry: https://github.com/tauri-apps/wry#platform-specific-notes
 # Install libgtk-3-dev and libxdo-dev for muda: https://github.com/tauri-apps/muda#dependencies-linux-only
-apt-get install -y --no-install-recommends libwebkit2gtk-4.1-dev libgtk-3-dev libxdo-dev
+apt-get install -y -q --no-install-recommends libwebkit2gtk-4.1-dev libgtk-3-dev libxdo-dev
