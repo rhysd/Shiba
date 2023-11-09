61c8411169b4ce63fbf76b7b3e2665a6f0bac8d7
rhysd <lin90162@yahoo.co.jp>
2023-11-09 19:45:25 +0900

    set product description on Windows explicitly

--- v2/build.rs
+++ v2/build.rs
@@ -4,6 +4,7 @@ fn main() -> std::io::Result<()> {
         winresource::WindowsResource::new()
             .set_icon("assets/icon.ico")
             .set("ProductName", "Shiba")
+            .set("FileDescription", "Shiba: Simple markdown previewer")
             .set("LegalCopyright", "Copyright (c) 2015 rhysd")
             .compile()?;
     }
