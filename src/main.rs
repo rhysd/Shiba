f3a7949331c9dda55c773bdb6befcfbd3ea418c8
rhysd <lin90162@yahoo.co.jp>
2026-01-21 17:33:50 +0900

    fix error message is discarded in release mode on Windows

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,9 @@ use env_logger::{Builder, Env};
 use log::LevelFilter;
 use shiba_preview::{Options, Parsed, run};
 use std::env;
+use std::process;
 
-fn main() -> Result<()> {
-    // When windows_subsystem is set to "windows", console outputs are detached from the process and no longer printed
-    // on terminal. However we still want to see the logger output for debugging by running this binary from terminal.
-    #[cfg(all(windows, not(debug_assertions)))]
-    let _console = shiba_preview::WindowsConsole::attach();
-
+fn try_main() -> Result<()> {
     match Options::parse(env::args_os())? {
         Parsed::Options(options) => {
             let level = if options.debug { LevelFilter::Debug } else { LevelFilter::Info };
@@ -38,5 +34,23 @@ fn main() -> Result<()> {
             println!("{version}");
             Ok(())
         }
+    }
+}
+
+// Note: `fn main() -> Result<()>` doesn't work on Windows. When `Result::Err` is returned from `main`, the console
+// is already detatched on Windows (in release mode). In this case the error message is not output to console and users
+// cannot know what was the error.
+fn main() {
+    // When windows_subsystem is set to "windows", console outputs are detached from the process and no longer printed
+    // on terminal. However we still want to see the logger output for debugging by running this binary from terminal.
+    #[cfg(all(windows, not(debug_assertions)))]
+    let _console = shiba_preview::WindowsConsole::attach();
+
+    if let Err(err) = try_main() {
+        eprintln!("Error: {err}");
+        for cause in err.chain().skip(1) {
+            eprintln!("  Caused by: {cause}");
+        }
+        process::exit(1);
     }
 }
