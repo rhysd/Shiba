aefced83aef5858ee3e8b2330c8f56266e269e85
rhysd <lin90162@yahoo.co.jp>
2026-01-22 21:50:38 +0900

    use oxc to check generated `JSON.parse()` call is correct in fuzzing test

--- fuzz/fuzz_targets/parse_markdown.rs
+++ fuzz/fuzz_targets/parse_markdown.rs
@@ -3,6 +3,9 @@
 
 use arbitrary::{Arbitrary, Unstructured};
 use libfuzzer_sys::fuzz_target;
+use oxc_allocator::Allocator;
+use oxc_parser::Parser;
+use oxc_span::SourceType;
 use shiba_preview::bench::{MarkdownContent, MarkdownParser, RawMessageWriter};
 
 fuzz_target!(|data: &[u8]| {
@@ -16,5 +19,10 @@ fuzz_target!(|data: &[u8]| {
     let parser = MarkdownParser::new(&target, offset, ());
     let mut buf = Vec::new();
     let () = parser.write_to(&mut buf).unwrap();
-    let _ = String::from_utf8(buf).unwrap();
+    let src = String::from_utf8(buf).unwrap();
+    let allocator = Allocator::default();
+    let parser = Parser::new(&allocator, &src, SourceType::mjs());
+    let parsed = parser.parse();
+    assert!(parsed.errors.is_empty(), "{:?}", parsed.errors);
+    assert!(!parsed.panicked);
 });
