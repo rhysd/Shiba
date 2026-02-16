72ecbee3c28babd7ef5a3d4ed4e5f6713db1c6c3
rhysd <lin90162@yahoo.co.jp>
2026-02-16 21:05:54 +0900

    add more tests for persistent data management

--- src/persistent.rs
+++ src/persistent.rs
@@ -1,11 +1,11 @@
 use anyhow::{Context, Result};
-use serde::Serialize;
 use serde::de::DeserializeOwned;
+use serde::ser::Serialize;
 use std::fs;
 use std::path::{Path, PathBuf};
 
 pub trait PersistentData {
-    const FILE: &'static str;
+    const FILE: &str;
 }
 
 #[derive(Debug)]
@@ -15,21 +15,23 @@ pub struct DataDir {
 
 impl Default for DataDir {
     fn default() -> Self {
-        fn data_dir() -> Option<PathBuf> {
+        fn path() -> Option<PathBuf> {
             let mut dir = dirs::data_dir()?;
             dir.push("Shiba");
             fs::create_dir_all(&dir).ok()?;
-            log::debug!("Data directory: {dir:?}");
+            log::debug!("Default data directory: {dir:?}");
             Some(dir)
         }
-        Self { path: data_dir() }
+        Self { path: path() }
     }
 }
 
 impl DataDir {
     pub fn new(dir: impl Into<PathBuf>) -> Self {
         let dir = dir.into();
-        Self { path: dir.is_dir().then_some(dir) }
+        let path = dir.is_dir().then_some(dir);
+        log::debug!("Custom data directory path: {path:?}");
+        Self { path }
     }
 
     pub fn path(&self) -> Option<&Path> {
@@ -74,12 +76,42 @@ impl DataDir {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    struct Test(bool);
+
+    impl PersistentData for Test {
+        const FILE: &str = "test.json";
+    }
 
     #[test]
-    fn custom_dir() {
-        let dir = DataDir::new(Path::new("."));
-        assert!(dir.path.is_some());
-        let dir = DataDir::new(Path::new("this-directory-does-not-exist"));
+    fn save_and_load() {
+        let tmp = tempfile::tempdir().unwrap();
+        let dir = DataDir::new(tmp.path());
+        assert_eq!(dir.path(), Some(tmp.path()));
+        let expected = Test(true);
+        dir.save(&expected).unwrap();
+        let file = tmp.path().join("test.json");
+        assert!(file.exists(), "path={file:?}");
+        let actual = dir.load().unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn no_data_dir() {
+        let dir = DataDir::new("this-directory-does-not-exist");
         assert!(dir.path.is_none());
+        assert_eq!(dir.load::<Test>(), None);
+        dir.save(&Test(true)).unwrap(); // Does nothing and it's not an error
+    }
+
+    #[test]
+    fn default_dir() {
+        let dir = DataDir::default();
+        let Some(path) = dir.path() else {
+            return; // Data directory was not found
+        };
+        assert!(path.ends_with("Shiba"), "path={path:?}");
     }
 }
